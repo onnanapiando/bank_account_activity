933b7df23c49e08ed085c15cb7f65a97d303f9c6
onnanapiando <onnapiando@gmail.com>
2014-11-18 20:23:57 +0800

    white spaces

--- account.py
+++ account.py
@@ -2,6 +2,3 @@ class Account(object):
     def __init__(self, account_number, balance):
       self.account_number = account_number
       self.balance = balance
-
-    def withdraw(self, account_number, amt_withdraw):
-      pass
--- account_test.py
+++ account_test.py
@@ -7,11 +7,6 @@ class TestAccount(unittest.TestCase):
       self.assertEqual(account.account_number, "001")
       self.assertEqual(account.balance, 50)
     
-    def test_withdraw(self):
-      account = Account("001", 50)
-      # acct_withdraw = account.withdraw(001,20)
-      # self.assertEqual(account.account_number, "001")     
-      self.assertEqual(account.withdraw, (001,20))
 
 if __name__ == '__main__':
     unittest.main()
